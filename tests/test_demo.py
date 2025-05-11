0745521b9a115ead4aaafec7dc50a3a7d9be6ac2
Jamser Ali <jamserali0011@gmail.com>
2025-05-11 10:56:21 +0530

    git ignore

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -14,3 +14,4 @@ class TestDemo:
     def test_verify_login_functionality_using_csv(self, setup, testdata):
         home_page = HomePage(setup)
         home_page.verify_login_functionality(testdata['email'], testdata['password'])
+
